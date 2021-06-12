6cc25bb8d61081450e69900f461b33435869535c
rameshjayaraj <rameshjayaraj@gmail.com>
2021-06-12 19:51:31 +0530

    Eureka Discovery Server

---
+++ src/test/java/com/user/discovery/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.discovery.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
